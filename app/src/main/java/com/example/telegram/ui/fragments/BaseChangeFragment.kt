02c541bae2309817342d7355848d236c5b1cec70
makar <makarvlad82@gmail.com>
2020-06-15 22:40:21 +0300

    Upload Project

---
+++ app/src/main/java/com/example/telegram/ui/fragments/BaseChangeFragment.kt
@@ -0,0 +1,41 @@
+package com.example.telegram.ui.fragments
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.TextView
+import com.example.telegram.MainActivity
+import com.example.telegram.R
+import com.example.telegram.utilits.APP_ACTIVITY
+import com.example.telegram.utilits.hideKeyboard
+
+/**
+ * A simple [Fragment] subclass.
+ */
+open class BaseChangeFragment (layout:Int) : Fragment(layout) {
+    override fun onStart() {
+        super.onStart()
+        setHasOptionsMenu(true)
+        APP_ACTIVITY.mAppDrawer.disableDrawer()
+        hideKeyboard()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        (activity as MainActivity).menuInflater.inflate(R.menu.settings_menu_confirm, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings_confirm_change -> change()
+        }
+        return true
+    }
+
+    open fun change() {
+
+    }
+}
---
+++ app/src/main/java/com/example/telegram/ui/fragments/BaseFragment.kt
@@ -0,0 +1,24 @@
+package com.example.telegram.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.telegram.MainActivity
+import com.example.telegram.R
+import com.example.telegram.utilits.APP_ACTIVITY
+
+open class BaseFragment(layout: Int) : Fragment(layout) {
+    
+    override fun onStart() {
+        super.onStart()
+        APP_ACTIVITY.mAppDrawer.disableDrawer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        APP_ACTIVITY.mAppDrawer.enableDrawer()
+    }
+}
